3bdd8815e1aab1b732bff0cbd587621512ada980
seamussmith <student.141504@worcesterschools.net>
2021-09-22 08:03:16 -0400

    New plan: With Regex

--- src/main/DocumentValidator.java
+++ src/main/DocumentValidator.java
@@ -8,41 +8,19 @@ import java.util.regex.Pattern;
 
 public final class DocumentValidator
 {
-    static final String TAG_START = "<";
-    static final String TAG_END = ">";
-    static final String TAG_END_BLOCK = "/";
-    static final String TAG_SELF_END = "/>";
-    static final String TAG_BLOCK_END = "</";
+    static final Pattern START_TAG = Pattern.compile("^<([A-z0-9\"= \n\t]*?)>$");
+    static final Pattern SELF_ENCLOSE_TAG = Pattern.compile("^<\\/([A-z0-9\"= \\n\\t]*?)>$");
+    static final Pattern END_TAG = Pattern.compile("^<([A-z0-9\"= \n\t]*?)\\/>$");
     static final Pattern VALID_CHARS_REGEX = Pattern.compile("[A-z]|\\-|\\!|\\\"|\\'");
 
     public static boolean isValidDocument(Scanner documentStream)
     {
-        Stack<String> tokens = new Stack<String>();
         Stack<String> tags = new Stack<String>();
         String nextTag = "";
+        
         while (documentStream.hasNext())
         {
-            var next = documentStream.next();
-            if (next.equals(TAG_END))
-            {
-                if (tokens.peek().equals(TAG_END))
-                    return false;
-                tokens.add(next);
-            }
-            else if (next.equals(TAG_START))
-            {
-                if (tokens.peek().equals(TAG_END))
-                    return false;
-                tokens.add(next);
-            }
-            else if (VALID_CHARS_REGEX.asPredicate().test(next))
-            {
-                tags.add(next);
-            }
-            else
-            {
-                return false;
-            }
+            
         }
         return true;
     }
