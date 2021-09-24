c8da5a8701f9146d6102d73a485354fb5c37473d
seamussmith <student.141504@worcesterschools.net>
2021-09-24 08:50:13 -0400

    aaaaaa

--- src/main/App.java
+++ src/main/App.java
@@ -1,9 +1,11 @@
 package main;
 
+import java.util.Scanner;
+
 public class App
 {
     public static void main(String[] args) 
     {
-        System.out.println("Hello, World!");
+        System.out.println(DocumentValidator.isValidDocument(new Scanner(System.in)));
     }
 }
--- src/main/DocumentValidator.java
+++ src/main/DocumentValidator.java
@@ -1,55 +1,85 @@
 package main;
 
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
 import java.util.regex.Pattern;
 
 public final class DocumentValidator
 {
     static final Pattern START_TAG = Pattern.compile("^<([A-z0-9\"= \n\t]*?)>$");
-    static final Pattern SELF_ENCLOSE_TAG = Pattern.compile("^<\\/([A-z0-9\"= \\n\\t]*?)>$");
+    static final Pattern SELF_ENCLOSE_TAG = Pattern.compile("^<(\\/|\\!)([A-z0-9\"= \\n\\t]*?)>$");
     static final Pattern END_TAG = Pattern.compile("^<([A-z0-9\"= \n\t]*?)\\/>$");
     static final Pattern VALID_CHARS_REGEX = Pattern.compile("[A-z]|\\-|\\!|\\\"|\\'");
-
+    
     public static boolean isValidDocument(Scanner documentStream)
     {
         Stack<String> tags = new Stack<String>();
         String nextTag = "";
+        String tagName = "";
+        boolean tagIdentified = false;
         boolean selfEnclosing = false;
+        boolean closing = false;
         boolean tag = false;
         while (documentStream.hasNext())
         {
-            var next = documentStream.next();
-            if (next.startsWith("<\\"))
+            var next = documentStream.next().toCharArray();
+            for (var c : next)
             {
-                if (!tag)
+                switch (c)
                 {
-                    tags.push(nextTag);
-                    nextTag = "";
+                case '<':
+                    if (tag)
+                       return false;
+                    tag = true;
+                    nextTag += c;
+                    break;
+                case '/':
+                    if (!tagIdentified)
+                        selfEnclosing = true;
+                    else
+                        closing = true;
+                    nextTag += c;
+                    break;
+                case '>':
+                    nextTag += c;
+                    if (selfEnclosing)
+                    {
+                        var regexResult = false;
+                        regexResult = SELF_ENCLOSE_TAG.asPredicate().test(nextTag);
+                        if (!regexResult)
+                            return false;
+                    }
+                    else if (closing)
+                    {
+                        var regexResult = false;
+                        regexResult = END_TAG.asPredicate().test(nextTag);
+                        if (!regexResult)
+                            return false;
+                    }
+                    else
+                    {
+                        var regexResult = false;
+                        regexResult = START_TAG.asPredicate().test(nextTag);
+                        if (!regexResult)
+                            return false;
+                    }
+                    break;
+                default:
+                    if (tag && !tagIdentified && c != ' ')
+                        tagName += c;
+                    else if (c == ' ')
+                        tagIdentified = true;
+                    nextTag += c;
+                    break;
                 }
-                selfEnclosing = true;
-                tag = true;
-                nextTag += next;
-                continue;
-            }
-            else if (next.startsWith("<"))
-            {
-                if (!tag)
-                {
-                    tags.push(nextTag);
-                    nextTag = "";
-                }
-                tag = true;
-                nextTag += next;
-                continue;
-            }
-            else if (next.endsWith(">"))
-            {
-                
             }
         }
         return true;
     }
+    enum TagType
+    {
+        BEGIN,
+        END,
+        SELF_CLOSE
+    }
+    private record HTMLTag(String name, TagType type) {}
 }
